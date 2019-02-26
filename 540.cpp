0f3b2638694efbc4eab457aeb01e7d8870152d70
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2019-02-26 19:28:33 -0300

    Add files via upload

--- 540.cpp
+++ 540.cpp
@@ -11,24 +11,25 @@ using namespace std;
 int main(){
 	ios::sync_with_stdio(false); cout << fixed << setprecision(3);
 
-	int scenario = 0;
-	cin >> scenario;
-	for(int i = 0; i < scenario; i++){
-		cout << "Scenario #" << i << endl;
-		int teamID = 0, member = 0;
+	string in;
+	cin >> in;
+	for(int scenario = 1; in.compare("0") != 0; scenario++){
+		cout << "Scenario #" << scenario << endl;
+		int member = 0;
 		list<pair<int, list<int>>> queue;
 		list<pair<int, list<int>>>::iterator it;
 		unordered_map<int, int> teams;
-		string in;
-		cin >> in;
 		while(in.compare("STOP") != 0){
 			if(in[0] > '0' && in[0] <= '9'){
-				int n = stoi(in);
-				for(int j = 0; j < n; j++){
-					cin >> member;
-					teams.insert(make_pair(member, teamID));
+				int nTeams = stoi(in);
+				for(int teamID = 0; teamID < nTeams; teamID++){
+					cin >> in;
+					int n = stoi(in);
+					for(int j = 0; j < n; j++){
+						cin >> member;
+						teams.insert(make_pair(member, teamID));
+					}
 				}
-				teamID++;
 			}
 			else if(in.compare("ENQUEUE") == 0){
 				cin >> member;
@@ -47,15 +48,19 @@ int main(){
 				}
 			}
 			else if(in.compare("DEQUEUE") == 0){
-				it = queue.begin();
-				cout << it->second.front() << "\n";
-				it->second.pop_front();
-				if(queue.front().second.empty()){
-					queue.pop_front();
+				if(!queue.empty()){
+					it = queue.begin();
+					cout << it->second.front() << "\n";
+					it->second.pop_front();
+					if(queue.front().second.empty()){
+						queue.pop_front();
+					}
 				}
 			}
 			cin >> in;
 		}
+		cout << endl;
+		cin >> in;
 	}
 
 	return 0;
