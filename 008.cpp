e75aa789e69b0bcdc5f4a2ca59124b91794f013b
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2019-03-13 23:48:05 -0300

    slower but right

--- 008.cpp
+++ 008.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<vector>
+#include<set>
+#include<string>
 
 using namespace std;
 #define inf 0x3f3f3f3f
@@ -7,7 +9,7 @@ using namespace std;
 
 int n = 0;
 
-void print(vector<vector<bool>> &board){
+void print(vector<vector<int>> &board){
 	for(int i = 0; i < n; i++){
 		for(int j = 0; j < n; j++){
 			cout << board[i][j];
@@ -17,13 +19,13 @@ void print(vector<vector<bool>> &board){
 	cout << endl;
 }
 
-void safeFill(vector<vector<bool>> &board, int x, int y, bool tf){
-	if(x >= 0 && x < n && y >= 0 && y < n && board[x][y] != tf)
+void safeFill(vector<vector<int>> &board, int x, int y, int tf){
+	if(x >= 0 && x < n && y >= 0 && y < n && board[x][y] != tf && board[x][y] != 2)
 		board[x][y] = tf;
 }
 
-void filler(vector<vector<bool>> &board, int x, int y, bool tf){
-	safeFill(board, x, y, tf);
+void filler(vector<vector<int>> &board, int x, int y, int tf){
+	safeFill(board, x, y, 2);
 	for(int i = 0; i < n; i++){
 		safeFill(board, x+i, y+i, tf);//x
 		safeFill(board, x+i, y-i, tf);
@@ -36,20 +38,44 @@ void filler(vector<vector<bool>> &board, int x, int y, bool tf){
 	}
 }
 
-void queens(vector<vector<bool>> &board, int q, vector<int> &sol){
+int whatsOnThisLine(vector<vector<int>> &board, int l){
+	int openSlots = 0;
+	for(int i = 0; i < n; i++){
+		if(board[l][i] == 2)
+			return 2;
+		if(board[l][i] == 1)
+			openSlots = 1;
+	}
+	return openSlots;
+}
+
+set<string> sol;
+
+void queens(vector<vector<int>> &board, int q){
+	//print(board);
 	if(q == n){
-		for(auto i : sol)
-			cout << i+1 << " ";
-		cout << endl;
+		string solu = "";
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				if(board[i][j] == 2){
+					solu += to_string(j+1) + " ";
+					break;
+				}
+			}
+		}
+		sol.insert(solu);
+		return;
 	}
 	for(int i = 0; i < n; i++){
-		if(board[i][q]){
-			sol.push_back(i);
-			vector<vector<bool>> newBoard;
-			newBoard = board;
-			filler(newBoard, i, q, 0);
-			queens(newBoard, q+1, sol);
-			sol.pop_back();
+		if(whatsOnThisLine(board, i) == 1){//there are open slots and no queen
+			for(int j = 0; j < n; j++){
+				if(board[i][j]){
+					vector<vector<int>> newBoard;
+					newBoard = board;
+					filler(newBoard, i, j, 0);
+					queens(newBoard, q+1);
+				}
+			}
 		}
 	}
 }
@@ -60,12 +86,14 @@ int main(){
 	while(cin >> n){
 		cin >> x0 >> y0;
 		x0--; y0--;
-		vector<vector<bool>> board(n, vector<bool>(n, 1));
-		vector<int> sol;
-		sol.push_back(x0);
+		vector<vector<int>> board(n, vector<int>(n, 1));
 		filler(board, x0, y0, 0);
+		queens(board, 1);
 		cout << "Solution" << endl;
-		queens(board, 1, sol);
+		for(auto i : sol){
+			cout << i << endl;
+		}
+		sol.clear();
 	}
 	return 0;
 }
