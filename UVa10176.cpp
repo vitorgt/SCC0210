7a25f7b7ff6148e036b9f1e414bedfcd1de51955
Vitor Gratiere Torres <26410338+vitorgt@users.noreply.github.com>
2019-04-04 17:28:25 -0300

    done

---
+++ UVa10176.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+using namespace std;
+#define inf 0x3f3f3f3f
+
+int main(){
+	char a = 0;
+	int b = 0;
+	while(scanf("%c", &a) == 1){
+		if(a == '0')
+			b = (b << 1) % 131071;
+		else if(a == '1')
+			b = ((b << 1) + 1) % 131071;
+		else{
+			printf(b%131071 ? "NO\n" : "YES\n");
+			b = 0;
+		}
+	}
+	return 0;
+}
